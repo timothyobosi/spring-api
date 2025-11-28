758543230ed25b1f8d35d00fcf66440b5c42ca78
timothyobosi <timothyobosi8@gmail.com>
2025-11-28 10:44:50 +0300

    create table in superbase

--- src/main/java/com/example/authapi/entity/user.java
+++ src/main/java/com/example/authapi/entity/user.java
@@ -1,4 +1,53 @@
 package com.example.authapi.entity;
 
-public class user {
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class user implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    public String password;
+
+    @Override
+    public Collection<?extends GrantedAuthority> getAuthorities(){
+        return List.of(); //Add roles later
+    }
+
+    @Override
+    public String getUsername(){
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked(){
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired(){
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled(){
+        return true;
+    }
 }
