58cbccee2bd9df9d6a1b0e61bd112338ac896b37
timothyobosi <timothyobosi8@gmail.com>
2025-11-28 10:54:34 +0300

    interface for UserRepository

--- src/main/java/com/example/authapi/repository/UserRepository.java
+++ src/main/java/com/example/authapi/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.example.authapi.repository;
 
-public class UserRepository {
+import com.example.authapi.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository {
+    Optional<User> findByEmail(String email);
+    boolean existByEmail(String email);
+
 }
