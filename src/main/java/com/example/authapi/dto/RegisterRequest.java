68921215a19700a5c90796780a8a4d88ab469dcc
timothyobosi <timothyobosi8@gmail.com>
2025-11-28 11:31:35 +0300

    Register request validating

--- src/main/java/com/example/authapi/dto/RegisterRequest.java
+++ src/main/java/com/example/authapi/dto/RegisterRequest.java
@@ -1,4 +1,12 @@
 package com.example.authapi.dto;
 
-public class RegisterRequest {
-}
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+    @Email @NotBlank String email,
+    @NotBlank @Size(min = 6) String password,
+    @NotBlank String fullName
+){}
